93ceb2afb3ad11dac51256f4f5afaad90fabed76
Azamat Ubaidullauly <azamat@Azamats-MacBook-Air.local>
2026-02-01 13:21:30 +0500

    middlewares

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"net/http"
 	mw "restapi/internal/api/middlewares"
-	"restapi/internal/api/utils"
+	"time"
 )
 
 type user struct {
@@ -36,16 +36,54 @@ func studentsHandler(w http.ResponseWriter, r *http.Request) {
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
+	fmt.Fprint(w, "Students Endpoint")
 }
 
 func teachersHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Teachers endpoint accessed")
+	switch r.Method {
+	case http.MethodGet:
+		w.Write([]byte("Hello GET method in teachers routes"))
+	case http.MethodPost:
+		w.Write([]byte("Hello POST method in teachers routes"))
+	case http.MethodPut:
+		w.Write([]byte("Hello PUT method in teachers routes"))
+	case http.MethodPatch:
+		w.Write([]byte("Hello PATCH method in teachers routes"))
+	case http.MethodDelete:
+		w.Write([]byte("Hello DELETE method in teachers routes"))
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 	fmt.Fprint(w, "Teachers Endpoint")
 }
 
 func execsHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Executives endpoint accessed")
+	switch r.Method {
+	case http.MethodGet:
+		w.Write([]byte("Hello GET method in execs routes"))
+	case http.MethodPost:
+		fmt.Println("Query Parameters:", r.URL.Query())
+
+		w.Write([]byte("Hello POST method in execs routes"))
+	case http.MethodPut:
+		w.Write([]byte("Hello PUT method in execs routes"))
+	case http.MethodPatch:
+		w.Write([]byte("Hello PATCH method in execs routes"))
+	case http.MethodDelete:
+		w.Write([]byte("Hello DELETE method in execs routes"))
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 	fmt.Fprint(w, "Executives Endpoint")
+}
+
+func applyMiddlewares(h http.Handler, middlewares ...func(http.Handler) http.Handler) http.Handler {
+	for _, middleware := range middlewares {
+		h = middleware(h)
+	}
+	return h
 }
 
 func main() {
@@ -68,10 +106,20 @@ func main() {
 		MinVersion: tls.VersionTLS12,
 	}
 
-	secureMux := utils.applyMiddlewares(mux,
+	rl := mw.NewRateLimiter(100, 1*time.Minute)
+	hppOptions := mw.HPPOptions{
+		CheckQuery:                   true,
+		CheckBody:                    true,
+		CheckBodyOnlyForContentTypes: "application/x-www-form-urlencoded",
+		Whitelist:                    []string{"SortBy", "Asc"},
+	}
+	secureMux := applyMiddlewares(mux,
 		mw.SecurityHeaders,
 		mw.Cors,
+		rl.Limit,
+		mw.HPPMiddleware(hppOptions),
 		mw.ResponseTimeMiddleware,
+		mw.CompressionMiddleware,
 	)
 
 	server := &http.Server{
